4dc9509269fd74a691611ecee4a458acf76c06f6
Micah Maphet <micahmaphet@gmail.com>
2026-05-16 15:37:55 -0400

    added arguments to crytpo-rpc call

--- bitcore/rpc-cli.js
+++ bitcore/rpc-cli.js
@@ -17,9 +17,22 @@ if (pathIndex !== -1) {
   path = process.env.BITCORE_CONFIG_PATH;
 }
 
-const chain = args[0];
+const chain = args[0].toUpperCase();
 const network = args[1];
 const command = args[2];
+if (!(chain && network && command)) {
+  console.log(`Please specify chain, network, and command. Got: chain: ${chain} network: ${network} command: ${command}`);
+  process.exit(1);
+}
+args.splice(0, 3);
+const rpcArgs = {};
+for (let i = 0; i < args.length; i++) {
+  if (!args[i].startsWith('--'))
+    continue;
+  rpcArgs[args[i].slice(2)] = args[i + 1];
+  args.splice(i, 2);
+  i--;
+}
 
 const config = JSON.parse(fs.readFileSync(path)).bitcoreNode;
 const chainConfig = config.chains[chain];
@@ -45,4 +58,4 @@ const rpc = new CryptoRpc({
   pass: rpcConfig.password
 }).get(chain);
 
-console.log(await rpc[command]());
+console.log(await rpc[command](rpcArgs));
